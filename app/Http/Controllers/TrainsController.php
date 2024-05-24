5d7fb7816f24d0040493f67c94a41054348c1c60
RoccoCerro <rcerro02@gmail.com>
2024-05-24 15:47:40 +0200

    Add: print trains in the page

--- app/Http/Controllers/TrainsController.php
+++ app/Http/Controllers/TrainsController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Trains;
 
 class TrainsController extends Controller
 {
     public function trains()
     {
-        return view('trains');
+        $trains_all = Trains::all();
+        $trains = Trains::where('date','>=', date(now()->format('Y-m-d')))->get();
+
+
+        return view('trains', compact('trains'), compact('trains_all'));
     }
 }
--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -7,5 +7,21 @@
 </head>
 <body>
   <h1>Page Trains</h1>
+  @foreach ($trains_all as $train)
+    {{ $train->brand }}
+    <br>
+  @endforeach
+  
+  <p>tutti i treni</p>
+  {{ $trains_all }}
+
+  <br>
+  
+  <p>treni in partenza da oggi</p>
+  {{$trains}}
+
+  @dump($trains)
+  @dump($trains_all)
+  @dump(date(now()->format('Y-m-d')))
 </body>
 </html>
