d00517750369477d6c7ad19cb5ccc045f72b7f6a
RoccoCerro <rcerro02@gmail.com>
2024-05-24 16:16:54 +0200

    Refactor: html

--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -20,45 +20,45 @@
   <p>treni in partenza da oggi</p>
   <div class="container">
     <div class="row">
-    @foreach ($trains as $train)
-    <div class="col">
-      <table>
-        <tr>
-          <td>brand</td>
+      <div class="col">
+        <table>
+          <tr>
+            <td>brand</td>
+        
+            <td>arrival_station</td>
+         
+            <td>departure_station</td>
+         
+            <td>date</td>
+        
+            <td>departure_time</td>
+         
+            <td>arrival_time</td>
       
-          <td>arrival_station</td>
-       
-          <td>departure_station</td>
-       
-          <td>date</td>
+            <td>train_code</td>
+        
+            <td>number_of_carriages</td>
       
-          <td>departure_time</td>
-       
-          <td>arrival_time</td>
-    
-          <td>train_code</td>
-      
-          <td>number_of_carriages</td>
-    
-          <td>in_time</td>
-      
-          <td>deleted</td>
-        </tr>
-        <tr>
-          <td>{{ $train->brand }}</td>
-          <td>{{ $train->arrival_station}}</td>
-          <td>{{ $train->departure_station }}</td>
-          <td>{{ $train->date }}</td>
-          <td>{{ $train->departure_time }}</td>
-          <td>{{ $train->arrival_time }}</td>
-          <td>{{ $train->train_code }}</td>
-          <td>{{ $train->number_of_carriages }}</td>
-          <td>{{ $train->in_time }}</td>
-          <td>{{ $train->deleted }}</td>
-        </tr>
-      </table>
-    </div>
-    @endforeach
+            <td>in_time</td>
+        
+            <td>deleted</td>
+          </tr>
+          @foreach ($trains as $train)
+          <tr>
+            <td>{{ $train->brand }}</td>
+            <td>{{ $train->arrival_station}}</td>
+            <td>{{ $train->departure_station }}</td>
+            <td>{{ $train->date }}</td>
+            <td>{{ $train->departure_time }}</td>
+            <td>{{ $train->arrival_time }}</td>
+            <td>{{ $train->train_code }}</td>
+            <td>{{ $train->number_of_carriages }}</td>
+            <td>{{ $train->in_time }}</td>
+            <td>{{ $train->deleted }}</td>
+          </tr>
+        </table>
+      </div>
+      @endforeach
     </div>
   </div>
 
