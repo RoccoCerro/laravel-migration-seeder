17740324467597c0a633ab28ac0ef9f3b7f6d6bd
RoccoCerro <rcerro02@gmail.com>
2024-05-24 17:45:50 +0200

    Refactor

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@yield("Title") - Treni</title>
+  @vite('resources/js/app.js')
+</head>
+
+  <body>
+
+
+  <main>
+    @yield("main")
+  </main>
+
+
+
+  </body>
+</html>
--- resources/views/trains.blade.php
+++ resources/views/trains.blade.php
@@ -1,66 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <style>
-    tr:first-of-type{
-      font-weight: bold;
-    }
+@extends("layouts.app")
 
-    td{
-      padding: 10px;
-    }
-  </style>
-</head>
-<body>
-  <h1>Page Trains</h1>
+@section("main")
+<h1>Page Trains</h1>
 
-  <p>treni in partenza da oggi</p>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <table>
-          <tr>
-            <td>brand</td>
-        
-            <td>arrival_station</td>
-         
-            <td>departure_station</td>
-         
-            <td>date</td>
-        
-            <td>departure_time</td>
-         
-            <td>arrival_time</td>
-      
-            <td>train_code</td>
-        
-            <td>number_of_carriages</td>
-      
-            <td>in_time</td>
-        
-            <td>deleted</td>
-          </tr>
-          @foreach ($trains as $train)
-          <tr>
-            <td>{{ $train->brand }}</td>
-            <td>{{ $train->arrival_station}}</td>
-            <td>{{ $train->departure_station }}</td>
-            <td>{{ $train->date }}</td>
-            <td>{{ $train->departure_time }}</td>
-            <td>{{ $train->arrival_time }}</td>
-            <td>{{ $train->train_code }}</td>
-            <td>{{ $train->number_of_carriages }}</td>
-            <td>{{ $train->in_time }}</td>
-            <td>{{ $train->deleted }}</td>
-          </tr>
-        </table>
-      </div>
-      @endforeach
-    </div>
-  </div>
+<p>treni in partenza da oggi</p>
+<div class="container">
+  <table class="my_table">
+    <tr>
+      <td>brand</td>
+  
+      <td>arrival_station</td>
+    
+      <td>departure_station</td>
+    
+      <td>date</td>
+  
+      <td>departure_time</td>
+    
+      <td>arrival_time</td>
 
-</body>
-</html>
+      <td>train_code</td>
+  
+      <td>number_of_carriages</td>
+
+      <td>in_time</td>
+  
+      <td>deleted</td>
+    </tr>
+    @foreach ($trains as $train)
+    <tr class="my_table-content">
+      <td class="padding15">{{ $train->brand }}</td>
+      <td class="padding15">{{ $train->arrival_station}}</td>
+      <td class="padding15">{{ $train->departure_station }}</td>
+      <td class="padding15">{{ $train->date }}</td>
+      <td class="padding15">{{ $train->departure_time }}</td>
+      <td class="padding15">{{ $train->arrival_time }}</td>
+      <td class="padding15">{{ $train->train_code }}</td>
+      <td class="padding15">{{ $train->number_of_carriages }}</td>
+      <td class="padding15">{{ $train->in_time }}</td>
+      <td class="padding15">{{ $train->deleted }}</td>
+    </tr>
+  </table>
+  @endforeach
+</div>
+@endsection
